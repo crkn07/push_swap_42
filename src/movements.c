b161307ebf291419a50a8ddbb006a609dd31ee39
Cristian Torres Bogota <crtorres@student.42madrid.com>
2023-04-05 17:49:19 +0200

    movements functions changed

--- src/movements.c
+++ src/movements.c
@@ -6,7 +6,7 @@
 /*   By: crtorres <crtorres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 19:04:11 by crtorres          #+#    #+#             */
-/*   Updated: 2023/04/05 13:34:00 by crtorres         ###   ########.fr       */
+/*   Updated: 2023/04/05 17:43:21 by crtorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,13 +59,77 @@
 	write(1, "ss\n", 3);
  }
 
+/**
+ * The function rotates a stack by moving the first element to the end.
+ * 
+ * @param stack A pointer to a stack, which is a linked list of integers.
+ * @param c The character 'c' represents which stack to rotate, either stack A or
+ * stack B. If 'c' is 'a', then stack A is rotated, and if 'c' is 'b', then stack B
+ * is rotated.
+ */
 void	ft_rotate(t_stack **stack, char c)
 {
 	t_stack	*last;
 	t_stack	*tmp;
 	
 	(*tmp) = (*stack);
-		
+	(*last) = ft_listlast(tmp);
+	last->next = (*stack);
+	(*stack) = (*tmp)->next;
+	tmp->next = NULL;
+	if (c == 'a')
+		write(1, "ra\n", 3);
+	else if (c == 'b')
+		write(1, "rb\n", 3);
+}
+
+/**
+ * The function `ft_rotate_ab` rotates both stacks `stack_a` and `stack_b` and
+ * prints "rr\n".
+ * 
+ * @param stack_a A pointer to a stack representing stack A.
+ * @param stack_b The parameter `stack_b` is a pointer to a pointer to a 
+ * `t_stack` struct. It is likely part of a larger program that implements 
+ * a stack data structure using linked lists. The `ft_rotate_ab` function takes
+ * two stacks (`stack_a` and `stack_b`) and rotates them
+ */
+void	ft_rotate_ab(t_stack **stack_a, t_stack **stack_b)
+{
+	ft_rotate(stack_a, NULL);
+	ft_rotate(stack_b, NULL);
+	write(1, "rr\n", 3);
+}
+
+void	ft_reverse_rotate(t_stack **stack, char c)
+{
+	t_stack	*last;
+	t_stack	*tmp;
+
+	tmp = *stack;
+	last = ft_stacklast(tmp);
+	while (tmp->next != NULL && tmp->next != last)
+		tmp = tmp->next;
+	tmp->next = NULL;
+	ft_stackadd_front(stack, last);
+	free(last);
+}
+
+/**
+ * The function adds a new node to the front of a stack.
+ * 
+ * @param stack A pointer to a pointer to the top node of a stack. This allows the
+ * function to modify the stack by adding a new node to the front.
+ * @param new The parameter `new` is a pointer to a `t_stack` structure, which
+ * contains a `number` field and a `next` field. It represents the node that we
+ * want to add to the front of the stack.
+ */
+void	ft_stackadd_front(t_stack **stack, t_stack *new)
+{
+	t_stack	*new_node;
+
+	new_node = ft_newnode(new->number);
+	new_node->next = *stack;
+	*stack = new_node;
 }
 /**
  * The function `ft_pa` moves the top element from stack B to stack A.
